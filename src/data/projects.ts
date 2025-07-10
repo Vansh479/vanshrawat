614edcf0d988e063e7d1eae0a51ec20d7f929cef
Vansh479 <146353231+Vansh479@users.noreply.github.com>
2025-07-10 16:34:10 +0530

    update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,12 +6,28 @@ export type Project = {
 };
 
 const projects: Project[] = [
+  
+  {
+    title: "ConvoFlow- Full Stack Real-time Chat Application",
+    techs: ["Next.js 14","React","Tailwind","Socket.IO","Lucia Auth","Drizzle ORM","Supabase","React Query"],
+    link:  "https://github.com/Vansh479/ConvoFlow",
+  },
+  {
+    title: "CipherThreads– Parallel File Encryptor/Decryptor",
+    techs: ["React", "Node.js", "Express", "Tailwind CSS", "Recharts" ],
+    link:  "https://github.com/Vansh479/CipherThreads",
+  },
+  {
+    title: "Fintrack- Parallelized Market Data Analyzer",
+    techs: ["Python", "YahooFinance", "Pandas"],
+    link: "https://github.com/Vansh479/FinTrack",
+  },
   {
     title: "Portfolio Theme using Astro",
     techs: ["Astro", "Cloudfare", "Tailwind"],
     link: "https://github.com/Vansh479/vanshrawat",
   },
   
-];
+  ];
 
 export default projects;
